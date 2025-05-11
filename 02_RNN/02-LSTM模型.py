b3c814a24a76688ed033f8266f239921fc118a5c
realyinan <19981014liu@gmail.com>
2025-05-11 22:32:05 +0800

    20250511

--- 02_RNN/02-LSTM模型.py
+++ 02_RNN/02-LSTM模型.py
@@ -17,9 +17,26 @@ def lstm_use():
     print("cn: \n", cn)
 
 
+def bilstm_use():
+    # 实例化对象
+    lstm = nn.LSTM(input_size=5, hidden_size=3, num_layers=1, batch_first=True, bidirectional=True)
+
+    # 输入
+    input = torch.randn(4, 4, 5)
+    h0 = torch.randn(2, 4, 3)
+    c0 = torch.randn(2, 4, 3)
+
+    # 将数据送入模型
+    output, (hn, cn) = lstm(input, (h0, c0))
+    print("output: \n", output)
+    print("hn: \n", hn)
+    print("cn: \n", cn)
+
+
 
 if __name__ == "__main__":
-    lstm_use()
+    # lstm_use()
+    bilstm_use()
 
 
 
